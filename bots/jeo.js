af2ad8da7af8e3ba82442a4047f521fc4ca81e09
Rose Baker <rosewin.baker@gmail.com>
2020-10-03 12:41:38 -0500

    interaction

--- bots/jeo.js
+++ bots/jeo.js
@@ -35,7 +35,23 @@ function jeo(message) {
 
         message.channel.send(categoryMessage);
         message.channel.send(question);
-        message.channel.send(titleCase(answer));
+        // message.channel.send(titleCase(answer));
+
+        // const quiz = require('./quiz.json');
+        // const item = quiz[Math.floor(Math.random() * quiz.length)];
+        const filter = response => {
+            return answer.some(answer => answer.toLowerCase() === response.content.toLowerCase());
+        };
+
+        message.channel.send(question).then(() => {
+            message.channel.awaitMessages(filter, { max: 1, time: 30000, errors: ['time'] })
+                .then(collected => {
+                    message.channel.send(`${collected.first().author} got the correct answer!`);
+                })
+                .catch(collected => {
+                    message.channel.send('Looks like nobody got the answer this time.');
+                });
+            });
     })
 
     
