41fd8980f652a834319f978d5e408fc48d6d0e74
Rose Baker <rosewin.baker@gmail.com>
2020-10-03 11:26:04 -0500

    q&a

--- bots/jeo.js
+++ bots/jeo.js
@@ -5,7 +5,11 @@ function jeo(message) {
     request('http://jservice.io/api/random', { json: true }, (err, res, body) => {
         if (err) { return console.log(err); }
 
-        console.log(body[0].question)
+        var question = body[0].question
+        var answer = body[0].answer
+
+        message.channel.send(question);
+        message.channel.send(answer);
     })
 
     
