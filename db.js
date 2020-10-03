e8d865cdbc8c780662c56737d8ca667cbbba7ada
Rose Baker <rosewin.baker@gmail.com>
2020-10-03 14:36:05 -0500

    db

---
+++ db.js
@@ -0,0 +1,23 @@
+const { Client } = require('pg');
+
+const client = new Pool({
+    connectionString: process.env.DATABASE_URL,
+    ssl: process.env.DATABASE_URL ? true : false
+})
+
+// const client = new Client({
+//   connectionString: process.env.DATABASE_URL,
+//   ssl: {
+//     rejectUnauthorized: false
+//   }
+// });
+
+client.connect();
+
+client.query('SELECT table_schema,table_name FROM information_schema.tables;', (err, res) => {
+  if (err) throw err;
+  for (let row of res.rows) {
+    console.log(JSON.stringify(row));
+  }
+  client.end();
+});
