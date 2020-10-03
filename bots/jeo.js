df861fd011726a063bff0d319946504e44c4eb3d
Rose Baker <rosewin.baker@gmail.com>
2020-10-03 12:02:28 -0500

    val

--- bots/jeo.js
+++ bots/jeo.js
@@ -8,7 +8,7 @@ function jeo(message) {
         var question = body[0].question
         var answer = body[0].answer
         var category = body[0].category.title
-        var value = body[0].category.value
+        var value = body[0].value
 
         message.channel.send("Category is " + category + " for $" + value);
         message.channel.send(question);
