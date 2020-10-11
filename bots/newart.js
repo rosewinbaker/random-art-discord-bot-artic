38230644f57ac9353309931da6046791759bae7c
Rose Baker <rosewin.baker@gmail.com>
2020-10-11 01:47:00 -0500

    embed

--- bots/newart.js
+++ bots/newart.js
@@ -22,26 +22,49 @@ const ctx = canvas.getContext('2d')
 //     // console.log(imageDataURL);
 // }
 
-function newart(message) {
-    // createImage();
+ctx.fillStyle = "blue";
+ctx.fillRect(0, 0, canvas.width, canvas.height);
 
-    ctx.fillStyle = "blue";
-    ctx.fillRect(0, 0, canvas.width, canvas.height);
+ctx.beginPath();
+ctx.arc(75, 75, 50, 0, Math.PI * 2, true); // Outer circle
+ctx.moveTo(110, 75);
+ctx.arc(75, 75, 35, 0, Math.PI, false);  // Mouth (clockwise)
+ctx.moveTo(65, 65);
+ctx.arc(60, 65, 5, 0, Math.PI * 2, true);  // Left eye
+ctx.moveTo(95, 65);
+ctx.arc(90, 65, 5, 0, Math.PI * 2, true);  // Right eye
+ctx.stroke();
 
-    ctx.beginPath();
-    ctx.arc(75, 75, 50, 0, Math.PI * 2, true); // Outer circle
-    ctx.moveTo(110, 75);
-    ctx.arc(75, 75, 35, 0, Math.PI, false);  // Mouth (clockwise)
-    ctx.moveTo(65, 65);
-    ctx.arc(60, 65, 5, 0, Math.PI * 2, true);  // Left eye
-    ctx.moveTo(95, 65);
-    ctx.arc(90, 65, 5, 0, Math.PI * 2, true);  // Right eye
-    ctx.stroke();
+var imageDataURL = canvas.toDataURL();
 
-    var imageDataURL = canvas.toDataURL();
-    console.log(imageDataURL);
+console.log(imageDataURL);
 
-    message.channel.send("My Bot's message", {files: [imageDataURL]});
-};
+const embed = new Discord.MessageEmbed() // Ver 12.2.0 of Discord.js
+    .setTitle("This is a title")
+    .setImage(imageDataURL)
+    .setThumbnail(imageDataURL)
+    message.channel.send(embed) 
+
+// function newart(message) {
+//     // createImage();
+
+//     ctx.fillStyle = "blue";
+//     ctx.fillRect(0, 0, canvas.width, canvas.height);
+
+//     ctx.beginPath();
+//     ctx.arc(75, 75, 50, 0, Math.PI * 2, true); // Outer circle
+//     ctx.moveTo(110, 75);
+//     ctx.arc(75, 75, 35, 0, Math.PI, false);  // Mouth (clockwise)
+//     ctx.moveTo(65, 65);
+//     ctx.arc(60, 65, 5, 0, Math.PI * 2, true);  // Left eye
+//     ctx.moveTo(95, 65);
+//     ctx.arc(90, 65, 5, 0, Math.PI * 2, true);  // Right eye
+//     ctx.stroke();
+
+//     var imageDataURL = canvas.toDataURL();
+//     console.log(imageDataURL);
+
+//     message.channel.send("My Bot's message", {files: [imageDataURL]});
+// };
 
 module.exports = newart
