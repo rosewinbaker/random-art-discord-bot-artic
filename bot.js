ca396f421f32edc3f8eeedc95ede00b62ec5bc0d
Rose Baker <rosewin.baker@gmail.com>
2020-10-11 02:15:39 -0500

    direct file

--- bot.js
+++ bot.js
@@ -8,7 +8,10 @@ var wordbot = require('./bots/word.js');
 var heyoo = require('./bots/heyo.js');
 var jeo = require('./bots/jeo.js');
 var newart = require('./bots/newart.js');
-import { imageDataURL } from "./bots/newart.js";
+// import { imageDataURL } from "./bots/newart.js";
+const { createCanvas, loadImage } = require('canvas')
+const canvas = createCanvas(50, 50)
+const ctx = canvas.getContext('2d')
 
 
 client.on("ready", () => {
@@ -39,6 +42,20 @@ client.on("message", (message) => {
   // Jeopardy question
   else if (message.content.startsWith("!newart")) {
     newart(message);
+    ctx.fillStyle = "blue";
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
+
+    ctx.beginPath();
+    ctx.arc(75, 75, 50, 0, Math.PI * 2, true); // Outer circle
+    ctx.moveTo(110, 75);
+    ctx.arc(75, 75, 35, 0, Math.PI, false);  // Mouth (clockwise)
+    ctx.moveTo(65, 65);
+    ctx.arc(60, 65, 5, 0, Math.PI * 2, true);  // Left eye
+    ctx.moveTo(95, 65);
+    ctx.arc(90, 65, 5, 0, Math.PI * 2, true);  // Right eye
+    ctx.stroke();
+
+    var imageDataURL = canvas.toDataURL();
 
     console.log(imageDataURL);
     
