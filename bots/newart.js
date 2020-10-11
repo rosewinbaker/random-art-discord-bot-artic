a913677f5d0d700b8eb22ebd3c00313c6372e934
Rose Baker <rosewin.baker@gmail.com>
2020-10-11 01:26:18 -0500

    imageDataURL

--- bots/newart.js
+++ bots/newart.js
@@ -2,8 +2,29 @@ const { createCanvas, loadImage } = require('canvas')
 const canvas = createCanvas(500, 500)
 const ctx = canvas.getContext('2d')
 
-function createImage() {
-    // Background blue
+// function createImage() {
+//     // Background blue
+//     ctx.fillStyle = "blue";
+//     ctx.fillRect(0, 0, canvas.width, canvas.height);
+
+//     ctx.beginPath();
+//     ctx.arc(75, 75, 50, 0, Math.PI * 2, true); // Outer circle
+//     ctx.moveTo(110, 75);
+//     ctx.arc(75, 75, 35, 0, Math.PI, false);  // Mouth (clockwise)
+//     ctx.moveTo(65, 65);
+//     ctx.arc(60, 65, 5, 0, Math.PI * 2, true);  // Left eye
+//     ctx.moveTo(95, 65);
+//     ctx.arc(90, 65, 5, 0, Math.PI * 2, true);  // Right eye
+//     ctx.stroke();
+
+//     var imageDataURL = canvas.toDataURL();
+
+//     // console.log(imageDataURL);
+// }
+
+function newart(message) {
+    // createImage();
+
     ctx.fillStyle = "blue";
     ctx.fillRect(0, 0, canvas.width, canvas.height);
 
@@ -18,12 +39,8 @@ function createImage() {
     ctx.stroke();
 
     var imageDataURL = canvas.toDataURL();
-
     console.log(imageDataURL);
-}
 
-function newart(message) {
-    createImage();
     message.channel.send("My Bot's message", {files: [imageDataURL]});
 };
 
