9d6bf5a21a0a1f70af3f40f69025b7d59ce51acd
Rose Baker <rosewin.baker@gmail.com>
2020-10-13 19:13:39 -0500

    queen

--- bot.js
+++ bot.js
@@ -7,7 +7,7 @@ var artbot = require('./bots/art.js');
 var wordbot = require('./bots/word.js');
 var heyoo = require('./bots/heyo.js');
 var jeo = require('./bots/jeo.js');
-var jeo = require('./bots/queen.js');
+var queen = require('./bots/queen.js');
 var newart = require('./bots/newart.js');
 // import { imageDataURL } from "./bots/newart.js";
 const { createCanvas, loadImage } = require('canvas')
