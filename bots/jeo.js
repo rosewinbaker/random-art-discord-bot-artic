88af3bea4e740b346cfd800c995a839e21bc1e1e
Rose Baker <rosewin.baker@gmail.com>
2020-10-25 19:18:35 -0500

    test

--- bots/jeo.js
+++ bots/jeo.js
@@ -66,45 +66,45 @@ function jeo(message) {
 
             
 
-        // // Create canvas with Jeopardy style text
-        // const { createCanvas } = require('canvas')
-        // const canvas = createCanvas(500, 500)
-        // const ctx = canvas.getContext('2d')
-        // ctx.fillStyle = '#060CE9';
-        // ctx.fillRect(0, 0, canvas.width, canvas.height);
+        // Create canvas with Jeopardy style text
+        const { createCanvas } = require('canvas')
+        const canvas = createCanvas(500, 500)
+        const ctx = canvas.getContext('2d')
+        ctx.fillStyle = '#060CE9';
+        ctx.fillRect(0, 0, canvas.width, canvas.height);
 
-        // // Pass the entire Canvas object because you'll need to access its width, as well its context
-        // const applyText = (canvas, text) => {
+        // Pass the entire Canvas object because you'll need to access its width, as well its context
+        const applyText = (canvas, text) => {
 
-        //     // Declare a base size of the font
-        //     let fontSize = 70;
+            // Declare a base size of the font
+            let fontSize = 70;
 
-        //     do {
-        //         // Assign the font to the context and decrement it so it can be measured again
-        //         ctx.font = `${fontSize -= 10}px sans-serif`;
-        //         // Compare pixel width of the text to the canvas minus the approximate avatar size
-        //     } while (ctx.measureText(text).width > canvas.width - 300);
+            do {
+                // Assign the font to the context and decrement it so it can be measured again
+                ctx.font = `${fontSize -= 10}px sans-serif`;
+                // Compare pixel width of the text to the canvas minus the approximate avatar size
+            } while (ctx.measureText(text).width > canvas.width - 300);
 
-        //     // Return the result to use in the actual canvas
-        //     return ctx.font;
-        // };
+            // Return the result to use in the actual canvas
+            return ctx.font;
+        };
 
     
-        // ctx.fillStyle = 'white';
-        // ctx.textAlign = "center";
-        // ctx.font = '50 px Helvetica';
+        ctx.fillStyle = 'white';
+        ctx.textAlign = "center";
+        ctx.font = '50 px Helvetica';
 
-        // // Add an exclamation point here and below
-        // // ctx.font = applyText(canvas, question);
-        // ctx.fillStyle = '#ffffff';
-        // ctx.fillText(`${question}!`, canvas.width / 2.5, canvas.height / 1.8);
+        // Add an exclamation point here and below
+        // ctx.font = applyText(canvas, question);
+        ctx.fillStyle = '#ffffff';
+        ctx.fillText(`${question}!`, canvas.width / 2.5, canvas.height / 1.8);
 
 
-        // // ctx.fillText(question, canvas.width / 2, canvas.height / 2);
+        // ctx.fillText(question, canvas.width / 2, canvas.height / 2);
 
-        // const attachment = new Discord.MessageAttachment(canvas.toBuffer(), 'jeopardy.png');
+        const attachment = new Discord.MessageAttachment(canvas.toBuffer(), 'jeopardy.png');
 
-        // message.channel.send(attachment);
+        message.channel.send(attachment);
 
 
         // message.channel.send(categoryMessage + " - originally aired " + finalAirDate);
