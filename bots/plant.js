c1516e98af64879f85c9fb9195f3e1de71121f22
Rose Baker <rosewin.baker@gmail.com>
2020-10-30 23:21:12 -0500

    embed

--- bots/plant.js
+++ bots/plant.js
@@ -36,10 +36,58 @@ function plant(message) {
                 // synonyms[]
                 // genus
                 // family 
+                // distributions.native[]
 
                 request(newAPIlink, { json: true }, (err, res, body) => {
                     if (err) { return console.log(err); }
                     message.channel.send(body.data.scientific_name);
+
+                    const embed = new Discord.MessageEmbed()
+
+
+                    if (body.data.main_species.image_url === null) {
+                        console.log("hey that image_url is null");
+                        }
+                        else {
+                        embed.setImage(body.data.main_species.image_url)
+                        }
+
+                    if (body.data.main_species.common_name === null) {
+                        console.log("hey that common_name is null");
+                        }
+                        else {
+                        embed.addField("Common Name", body.data.main_species.common_name, true)
+                        } 
+
+                    if (body.data.main_species.family_common_name === null) {
+                        console.log("hey that family_common_name is null");
+                        }
+                        else {
+                        embed.addField("Family Common Name", body.data.main_species.family_common_name, true)
+                        } 
+
+                    if (body.data.main_species.scientific_name === null) {
+                        console.log("hey that scientific_name is null");
+                        }
+                        else {
+                        embed.addField("Scientific Name", body.data.main_species.scientific_name, true)
+                        } 
+                    
+                    if (body.data.main_species.genus === null) {
+                        console.log("hey that genus is null");
+                        }
+                        else {
+                        embed.addField("Genus", body.data.main_species.genus, true)
+                        } 
+
+                    if (body.data.main_species.family === null) {
+                        console.log("hey that family is null");
+                        }
+                        else {
+                        embed.addField("Family", body.data.main_species.family, true)
+                        } 
+
+                        message.channel.send({embed});
 
             });
 
