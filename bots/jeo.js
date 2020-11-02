e1944c40b234818f88dfd26f7037308efe47b0df
Rose Baker <rosewin.baker@gmail.com>
2020-11-02 11:38:39 -0600

    title case

--- bots/jeo.js
+++ bots/jeo.js
@@ -1,6 +1,17 @@
 const { Client } = require('pg');
 const request = require('request');
 
+function titleCase(str) {
+    var splitStr = str.toLowerCase().split(' ');
+    for (var i = 0; i < splitStr.length; i++) {
+        // You do not need to check if i is larger than splitStr length, as your for does that for you
+        // Assign it back to the array
+        splitStr[i] = splitStr[i].charAt(0).toUpperCase() + splitStr[i].substring(1);     
+    }
+    // Directly return the joined string
+    return splitStr.join(' '); 
+ }
+
 function jeo(message) {
 
     request('http://jservice.io/api/random', { json: true }, (err, res, body) => {
