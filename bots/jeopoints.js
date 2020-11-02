b6b2fe98811b03725010f0aa685662499a00eccd
Rose Baker <rosewin.baker@gmail.com>
2020-11-02 15:14:58 -0600

    jeopoints res.rows[1].userid

--- bots/jeopoints.js
+++ bots/jeopoints.js
@@ -23,7 +23,7 @@ function pointsjeo(message) {
             return;
         }
 
-        console.log("Response is: " + res.row[1].userid);
+        console.log("Response is: " + res.rows[1].userid);
 
         client.end()
     });
